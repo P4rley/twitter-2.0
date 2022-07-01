004b129009a4718fc2a333c21fb900c072beba3e
P4rley <adwihersa@gmail.com>
2022-07-01 09:44:32 +0700

    First Commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return <div>Sidebar</div>;
+}
+export default Sidebar;
